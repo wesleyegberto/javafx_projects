6d52ccb873dabb6309bc098c90ae75b1bceed1e5
Wesley Egberto <wesleyegberto@gmail.com>
2016-05-05 23:37:11 -0300

    Formatted the commentary of the ImageViewPane

--- programming-block/src/main/java/com/github/wesleyegberto/programmingblock/component/ImageViewPane.java
+++ programming-block/src/main/java/com/github/wesleyegberto/programmingblock/component/ImageViewPane.java
@@ -1,9 +1,8 @@
-package com.github.wesleyegberto.programmingblock.component;
-/*
+/**
  * Copyright (c) 2012, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  */
-
+package com.github.wesleyegberto.programmingblock.component;
 
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
@@ -15,7 +14,6 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.Region;
 
 /**
- *
  * @author akouznet
  */
 class ImageViewPane extends Region {
